f7552ad08d2973006d2c0303ef7c450c00add4c9
satya-me <satyajit@kotaielectronics.com>
2022-09-21 15:17:19 +0530

    ..

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -3,26 +3,6 @@ class ControllerCommonHome extends Controller
 {
     public function index()
     {
-        $this->document->setTitle($this->config->get('config_meta_title'));
-        $this->document->setDescription($this->config->get('config_meta_description'));
-        $this->document->setKeywords($this->config->get('config_meta_keyword'));
-
-        if (isset($this->request->get['route'])) {
-            $this->document->addLink($this->config->get('config_url'), 'canonical');
-        }
-
-        $data['column_left'] = $this->load->controller('common/column_left');
-        $data['column_right'] = $this->load->controller('common/column_right');
-        $data['content_top'] = $this->load->controller('common/content_top');
-        $data['content_bottom'] = $this->load->controller('common/content_bottom');
-        $data['footer'] = $this->load->controller('common/footer');
-        $data['header'] = $this->load->controller('common/header');
-
-        $this->response->setOutput($this->load->view('common/home', $data));
-
-        // New code
-
-        // $data['assets'] = ASSET_URL;
         // $this->document->setTitle($this->config->get('config_meta_title'));
         // $this->document->setDescription($this->config->get('config_meta_description'));
         // $this->document->setKeywords($this->config->get('config_meta_keyword'));
@@ -31,48 +11,68 @@ class ControllerCommonHome extends Controller
         //     $this->document->addLink($this->config->get('config_url'), 'canonical');
         // }
 
-        // $data['column_left'] = $this->load->controller('web/common/column_left');
-        // $data['column_right'] = $this->load->controller('web/common/column_right');
-        // $data['content_top'] = $this->load->controller('web/common/content_top');
-        // $data['content_bottom'] = $this->load->controller('web/common/content_bottom');
-        // $data['footer'] = $this->load->controller('web/common/footer');
-        // $data['header'] = $this->load->controller('web/common/header');
+        // $data['column_left'] = $this->load->controller('common/column_left');
+        // $data['column_right'] = $this->load->controller('common/column_right');
+        // $data['content_top'] = $this->load->controller('common/content_top');
+        // $data['content_bottom'] = $this->load->controller('common/content_bottom');
+        // $data['footer'] = $this->load->controller('common/footer');
+        // $data['header'] = $this->load->controller('common/header');
 
-        // $this->load->model('setting/module');
+        // $this->response->setOutput($this->load->view('common/home', $data));
 
-        // $data['modules'] = array();
-        // $layout_id = 14;
-        // $modules = $this->model_design_layout->getLayoutModules($layout_id, 'content_top');
+        // New code
 
-        // foreach ($modules as $module) {
-        //     $part = explode('.', $module['code']);
+        $data['assets'] = ASSET_URL;
+        $this->document->setTitle($this->config->get('config_meta_title'));
+        $this->document->setDescription($this->config->get('config_meta_description'));
+        $this->document->setKeywords($this->config->get('config_meta_keyword'));
 
-        //     if (isset($part[0]) && $this->config->get('module_' . $part[0] . '_status')) {
-        //         $module_data = $this->load->controller('web/extension/module/' . $part[0]);
+        if (isset($this->request->get['route'])) {
+            $this->document->addLink($this->config->get('config_url'), 'canonical');
+        }
 
-        //         if ($module_data) {
-        //             if ($part[0] == 'banner') {
-        //                 $data['banner'][] = $module_data;
-        //             }
-        //         }
-        //     }
+        $data['column_left'] = $this->load->controller('web/common/column_left');
+        $data['column_right'] = $this->load->controller('web/common/column_right');
+        $data['content_top'] = $this->load->controller('web/common/content_top');
+        $data['content_bottom'] = $this->load->controller('web/common/content_bottom');
+        $data['footer'] = $this->load->controller('web/common/footer');
+        $data['header'] = $this->load->controller('web/common/header');
 
-        //     if (isset($part[1])) {
-        //         $setting_info = $this->model_setting_module->getModule($part[1]);
+        $this->load->model('setting/module');
 
-        //         if ($setting_info && $setting_info['status']) {
-        //             $output = $this->load->controller('web/extension/module/' . $part[0], $setting_info);
+        $data['modules'] = array();
+        $layout_id = 14;
+        $modules = $this->model_design_layout->getLayoutModules($layout_id, 'content_top');
 
-        //             if ($output) {
-        //                 if ($part[0] == 'banner') {
-        //                     $data['banner'][] = $output;
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
-        // // print_r($data['banner']);
+        foreach ($modules as $module) {
+            $part = explode('.', $module['code']);
 
-        // $this->response->setOutput($this->load->view('web/common/home', $data));
+            if (isset($part[0]) && $this->config->get('module_' . $part[0] . '_status')) {
+                $module_data = $this->load->controller('web/extension/module/' . $part[0]);
+
+                if ($module_data) {
+                    if ($part[0] == 'banner') {
+                        $data['banner'][] = $module_data;
+                    }
+                }
+            }
+
+            if (isset($part[1])) {
+                $setting_info = $this->model_setting_module->getModule($part[1]);
+
+                if ($setting_info && $setting_info['status']) {
+                    $output = $this->load->controller('web/extension/module/' . $part[0], $setting_info);
+
+                    if ($output) {
+                        if ($part[0] == 'banner') {
+                            $data['banner'][] = $output;
+                        }
+                    }
+                }
+            }
+        }
+        // print_r($data['banner']);
+
+        $this->response->setOutput($this->load->view('web/common/home', $data));
     }
 }
